6e7e2a55116c08d540a16fbea55af2834bcfd782
KevinPanUwU <kpan24@uwcchina.org>
2025-09-12 09:31:04 +0800

    Add files via upload

---
+++ l2.py
@@ -0,0 +1,28 @@
+# Strings
+first_name = "KP"
+food = "pizza"
+email = "kpan24@uwcchian.org"
+
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean
+is_student = True
+for_sale = False
+is_online = True
+
+print(f"Hello {first_name}")
+print(f"You are {age} years old")
+print(f"Your gpa is: {gpa}")
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT student")
